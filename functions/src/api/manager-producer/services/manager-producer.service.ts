d55b5213a177a72c5d0ad5b02163d9c33efe876a
william koller <wkoller25@gmail.com>
2021-04-07 16:22:56 -0300

    feat: startted sqs producer

--- functions/src/api/manager-producer/services/manager-producer.service.ts
+++ functions/src/api/manager-producer/services/manager-producer.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { config, SQS } from 'aws-sdk';
+import { Producer } from 'sqs-producer';
 
 @Injectable()
-export class ManagerProducerService {}
+export class ManagerProducerService {
+  private logger = new Logger(ManagerProducerService.name);
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly producer: Producer,
+  ) {
+    this.producer = Producer.create({
+      queueUrl: this.configService.get('MANAGER_PRODUCER_SQL'),
+      sqs: new SQS({
+        maxRetries: 10,
+        httpOptions: { timeout: 500000 },
+        region: this.configService.get('AWS_REGION'),
+      }),
+    });
+  }
+  async produce(): Promise<void> {
+    if (this.configService.get('MODE') === 'DEVELOP') {
+      config.update({
+        region: this.configService.get('AWS_REGION'),
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      });
+    }
+    try {
+      await this.producer.send('');
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+}
