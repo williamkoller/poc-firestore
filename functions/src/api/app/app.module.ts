f966970a6d75344754418e809412a28c15525365
william koller <wkoller25@gmail.com>
2021-04-07 15:53:20 -0300

    fix: organize files

--- functions/src/api/app/app.module.ts
+++ functions/src/api/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ProductModule } from '../../product/product.module';
+import { ProductModule } from '../product/product.module';
 import { AppController } from '../app/controllers/app.controller';
 import { AppService } from '../app/services/app.service';
 import { ManagerProducerModule } from '../manager-producer/manager-producer.module';
